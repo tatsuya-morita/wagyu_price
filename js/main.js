097d372b1d7ff1758f16f652c1bc221b30d95cb2
tatuya_morita <r.k.z.mota@gmail.com>
2022-06-01 22:01:15 +0900

    スマホサイズ時にトップスクロールボタンを表示する

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(function(){
-  $('#page-top').click(function(){
-    $('html, body').animate({ scrollTop: 0 }, 200);
-  })
-})
+  var pageSize = $(window).width();
+  var mobileSize = 576;
+  $('#page-top').hide();
+  if (pageSize <= mobileSize) {
+    var pageTop = $('#page-top')
+    $('#page-top').show();
+    $('#page-top').click(function(){
+      $('html, body').animate({ scrollTop: 0 }, 200);
+      if ($(this).scrolltop() > 100) {
+        pagetop.fadeIn();
+      } else {
+        pagetop.fadeOut();
+      }
+    })
+  };
+});
